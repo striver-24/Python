04c46fdb618d82a85909e36f2b96318ac69c624d
Deivyansh Singh <106349960+striver-24@users.noreply.github.com>
2024-06-27 11:24:56 +0530

    loops

---
+++ chainednnested.py
@@ -0,0 +1,34 @@
+# chained and nested 
+x = 2
+y = 3
+
+if x == y and x + y == 5:
+    print("x is equal to y and x + y is equal to 5")
+else:
+    print("x is not equal to y or x + y is not equal to 5")
+
+
+# for loops with patterns
+# *
+# **
+# ***
+# ****
+# *****
+# ******
+# *******
+# ********
+# *********
+    
+n = 10
+i = 0
+for i in range(n):
+    print('*' * i)
+
+    
+# for loops in range (start,stop,step)
+    
+''' while loop --> syntax
+        while condition == True:
+            do this
+            break
+'''
