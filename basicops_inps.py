de9fcd8f16b63a32e6d767b389882e1c0ef4c861
Deivyansh Singh <106349960+striver-24@users.noreply.github.com>
2024-06-27 10:46:24 +0530

    second-commit

---
+++ basicops_inps.py
@@ -0,0 +1,29 @@
+#Taking Input
+#print("What is your name?")
+#name = input()
+#print("Hello, " + name + "!")
+
+#+,-,*,/,%,**(exponential),//(gives lower round-off value)
+
+a = 5
+b = 7
+sum = a + b
+sub = a - b
+mul = a * b
+div = b / a
+rem = b % a
+exp = b ** a
+flo = b // a
+print(sum)
+print(sub)
+print(mul)
+print(div)
+print(rem)
+print(exp)
+print(flo)
+
+# concatenation
+name1 = "Deivyansh "
+name2 = "Singh Raghuvanshi"
+fullName = name1 + name2
+print(fullName)
---
+++ variables_datatypes.py
@@ -0,0 +1,19 @@
+# int -> any whole number
+# str -> anything between quotations
+# float -> anything with decimal
+# bool -> True or False
+
+a = 4
+b = "Hello"
+print(a) 
+if ( a==4 ):
+    bool_a = True
+    print(bool_a)
+else:
+    print("not true")
+
+#type 
+
+print(type(a))
+print(type(bool_a))
+print(type(b))
