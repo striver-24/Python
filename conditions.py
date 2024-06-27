0aaea280fdab17fdd00670dacd507d94642d1270
Deivyansh Singh <106349960+striver-24@users.noreply.github.com>
2024-06-27 11:05:53 +0530

    if/else/elif

--- conditions.py
+++ conditions.py
@@ -2,3 +2,15 @@
 
 print(5!=7)
 
+#if/else/elif
+
+# if condition == True:
+#     do this
+
+age = input("Input your age : ")
+if int(age) == 16:
+    print("You are 16")
+elif int(age) >= 16:
+    print("You are over 16")
+else:
+    print("You are minor")
